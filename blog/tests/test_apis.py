4c0e41b6000c4628ccff01dcf1e077b33edd9580
Soroosh Daryabari <110119235+SorooshDaryabari@users.noreply.github.com>
2023-01-17 21:14:44 +0330

    Delete test_apis.py

--- blog/tests/test_apis.py
+++
@@ -1,8 +0,0 @@
-from urllib import response
-
-
-def test_get_all_categories(api_client, category_with_multiple_children):
-    endpoint = "blog/categories/"
-    response = api_client().get(endpoint)
-    assert response.status_code == 200
-    assert len(response.data) == len(category_with_multiple_children)
